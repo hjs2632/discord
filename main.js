e04769e3ac8856184189c3690df22089bb0ceacb
sol0sol0 <44517049+hjs2632@users.noreply.github.com>
2022-09-11 13:53:47 +0900

    Edit | main code
    discord v14;;

--- main.js
+++ main.js
@@ -1,23 +1,14 @@
-// const Discord = require("discord.js");
-const { Client, GatewayIntentBits } = require('discord.js');
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds
-  ]
-});
+const Discord = require("discord.js");
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 const token = require("./config/token.json");
 
 client.once('ready', () => { 
   console.log(`Hello~ ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-      await interaction.reply('Pong!');
-  }
+client.on('messageCreate', msg => {
+  msg.reply("드디어 성공!");
 });
 
 client.login(token.token);
